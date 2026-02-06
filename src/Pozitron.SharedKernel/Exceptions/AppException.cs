7310ba667099922aa69a3fe977dfe013537c02f8
Fati Iseni <fati.iseni@pozitrongroup.com>
2026-02-06 13:31:22 +0100

    Add SharedKernel exceptions.

---
+++ src/Pozitron.SharedKernel/Exceptions/AppException.cs
@@ -0,0 +1,21 @@
+﻿namespace Pozitron.SharedKernel;
+
+public class AppException : Exception
+{
+    /// <summary>
+    /// Initializes a new instance of the AppException class with a specified error message.
+    /// </summary>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
+    public AppException(string message) : base(message)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the AppException class with a specified error message and an inner exception.
+    /// </summary>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
+    /// <param name="innerException">The exception that triggered this exception.</param>
+    public AppException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
---
+++ tests/Pozitron.SharedKernel.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Pozitron.SharedKernel.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
